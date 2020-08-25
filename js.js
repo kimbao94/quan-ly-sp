33adf8943c4cd6b8e1bee7b781cf070827c60bbe
unknown <nguyenkimbao361@gmail.com>
2020-08-25 16:04:20 +0700

    add

---
+++ js.js
@@ -0,0 +1,37 @@
+let productLish = ["Sony", "Samsung", "Nokia", "Xiaomi", "Apple", "Oppo"];
+
+function showAllProduct() {
+    let data = "<table border='1px'>";
+    data += "<tr>" +
+        "<td>Product Name</td>"+
+        "<td></td>" +
+        "</tr>";
+    for (let i = 0; i < productLish.length; i++) {
+        data+= "<tr>" +
+            "<td id='productName"+i+"'>"+productLish[i]+ "</td>"+
+            "<td>" +
+            "<button onclick='editProduct("+i+")'>Edit</button>" +
+            "<button onclick='deleteProduct("+ i +")'>Delete</button>" +
+            "</td>" +
+            "</tr>";
+    }
+    data += "</table>"
+
+    document.getElementById("listProdcut").innerHTML = data;
+}
+
+function addNewProduct() {
+    let newProductName = document.getElementById("newProduct").value;
+    productLish.push(newProductName);
+    showAllProduct();
+}
+
+function deleteProduct(index) {
+    productLish.splice(index, 1);
+    showAllProduct();
+}
+
+function editProduct(id) {
+    productLish[id] = prompt("nhập vào tên muốn sửa");
+    showAllProduct();
+}
