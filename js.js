6b81ac499571b2eeb57dc9e863d1446c839dc83a
kimbao94 <69283743+kimbao94@users.noreply.github.com>
2020-08-27 08:44:53 +0700

    edit

--- js.js
+++ js.js
@@ -3,15 +3,15 @@ let productLish = ["Sony", "Samsung", "Nokia", "Xiaomi", "Apple", "Oppo"];
 function showAllProduct() {
     let data = "<table border='1px'>";
     data += "<tr>" +
-        "<td>Product Name</td>"+
+        "<td>Product Name</td>" +
         "<td></td>" +
         "</tr>";
     for (let i = 0; i < productLish.length; i++) {
-        data+= "<tr>" +
-            "<td id='productName"+i+"'>"+productLish[i]+ "</td>"+
+        data += "<tr>" +
+            "<td id='productName" + i + "'>" + productLish[i] + "</td>" +
             "<td>" +
-            "<button onclick='editProduct("+i+")'>Edit</button>" +
-            "<button onclick='deleteProduct("+ i +")'>Delete</button>" +
+            "<button onclick='editProduct(" + i + ")' style='background-color: greenyellow'>Edit</button>" +
+            "<button onclick='deleteProduct(" + i + ")' style='background-color: greenyellow'>Delete</button>" +
             "</td>" +
             "</tr>";
     }
@@ -27,8 +27,10 @@ function addNewProduct() {
 }
 
 function deleteProduct(index) {
-    productLish.splice(index, 1);
-    showAllProduct();
+    if (confirm("bạn có chắc sẽ xóa ?")) {
+        productLish.splice(index, 1);
+        showAllProduct();
+    }
 }
 
 function editProduct(id) {
